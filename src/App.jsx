34654b43c7a141cecc7de890a7ab0d84e76ec483
Artem Tkachenko <shikams303@gmail.com>
2025-06-22 11:42:21 +0300

    lifting up some data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
-import Header from "./components/Header"
-import UserInput from "./components/UserInput"
+import { useState } from "react";
+import Header from "./components/Header";
+import UserInput from "./components/UserInput";
+import Results from "./components/Results";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1000,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
+
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      <Results />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
